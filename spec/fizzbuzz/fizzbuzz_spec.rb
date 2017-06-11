6ddafe0f0e755706f6ff2781eb6e8c8425592942
hendrik <g9a2hvk9@me.com>
2017-06-11 10:06:24 +0100

    Sets up rspec testing and requires files correctly

---
+++ spec/fizzbuzz/fizzbuzz_spec.rb
@@ -0,0 +1,3 @@
+require 'spec_helper' #requires spec_helper file
+require 'web_helper' #require web-helper to stream line rspec testing
+require './src/fizzbuzz/fizzbuzz.rb' # requires fizzbuzz source file for rspec testing
